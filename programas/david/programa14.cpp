363fefab8c999e68d4a3d6441da65ab3f057ba09
Davidmazariegos88 <jmazariegoso5@miumg.edu.gt>
2025-04-04 23:21:39 -0600

    Programa14

--- programas/david/programa14.cpp
+++ programas/david/programa14.cpp
@@ -1,17 +1,45 @@
 /*	
 *	Programa que mueve un punto en toda la pantalla
 *	
-*	Universidad Mariano Gálvez de Guatemala
-*	Facultad de Ingeniería en Sistemas
+*	Universidad Mariano Galvez de Guatemala
+*	Facultad de Ingenieria en Sistemas
 *	
 *	Autor:
-*	José David Mazariegos Ortega
+*	Jose David Mazariegos Ortega
 */
 
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
 
 using namespace std;
 
-void programaCatorce()
-{
+void programaCatorce(){
+	int x = 0, y = 0;
+	int dx = 1, dy = 1;
+	const int ANCHO = 119;
+	const int ALTO = 29;
+	
+	while (!kbhit()) {
+		system("cls");
+		
+		for (int i = 0; i < y; i++) cout << endl;
+		for (int j = 0; j < x; j++) cout << " ";
+		cout << "*";
+		
+		x += dx;
+		y += dy;
+		
+		if (x <= 0 || x >= ANCHO) dx = -dx;
+		if (y <= 0 || y >= ALTO) dy = -dy;
+		
+		Sleep(50);
+	}
 }
+
+int main() {
+	programaCatorce();
+	return 0;
+}
+	
+
