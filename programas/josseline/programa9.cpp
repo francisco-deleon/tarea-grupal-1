bf663fbc19025eb2423b96f95c0b7d1191c96477
Francisco Natareno <fdeleonn2@miumg.edu.gt>
2025-04-05 22:42:17 -0600

    Corrección de estructura del programa 9

--- programas/josseline/programa9.cpp
+++ programas/josseline/programa9.cpp
@@ -1,11 +1,11 @@
 /*	
 *	Programa que genera todas las tablas de multiplicar del 1 al 10
 *	
-*	Universidad Mariano G·lvez de Guatemala
-*	Facultad de IngenierÌa en Sistemas
+*	Universidad Mariano G√°lvez de Guatemala
+*	Facultad de Ingenier√≠a en Sistemas
 *	
 *	Autor:
-*	Josseline Adriana Catal·n Ju·rez
+*	Josseline Adriana Catal√°n Ju√°rez
 */
 
 #include <iostream>
@@ -13,15 +13,11 @@ using namespace std;
 
 void programaNueve()
 {
-}
-
-int main(){
-	for (int i=1;i<=10;i++){
-		cout<<"Tabla de multiplicar del "<<i<<":"<<endl;
-	for (int j=1;j<=10;j++)
-		cout<<i<<"x"<<j<<"="<<i*j<<endl;
+	for (int i=1; i<=10; i++){
+		cout << "Tabla de multiplicar del " << i << ":" << endl;
+	for (int j=1; j<=10; j++)
+		cout << i << "x" << j << " = " << i*j << endl;
 	}
 	
-	cout<<endl;
-	
+	cout << endl;
 }
