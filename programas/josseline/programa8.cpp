b2dc0ae483372e7f38e3a3f31a3640c3cc03674c
Francisco Natareno <fdeleonn2@miumg.edu.gt>
2025-04-05 22:38:33 -0600

    Corrección de estructura del programa 8

--- programas/josseline/programa8.cpp
+++ programas/josseline/programa8.cpp
@@ -1,30 +1,27 @@
 /*	
 *	Programa que genera una tabla de multiplicar
 *	
-*	Universidad Mariano G·lvez de Guatemala
-*	Facultad de IngenierÌa en Sistemas
+*	Universidad Mariano G√°lvez de Guatemala
+*	Facultad de Ingenier√≠a en Sistemas
 *	
 *	Autor:
-*	Josseline Adriana Catal·n Ju·rez
+*	Josseline Adriana Catal√°n Ju√°rez
 */
 
 #include <iostream>
+
 using namespace std;
 
 void programaOcho()
 {
-}
-
-int main(){
 	int numero;
 	
-	cout<<"Introducir un numero para crear tabla de multiplicar: ";
-	cin>>numero;
+	cout << "Introducir un numero para crear tabla de multiplicar: ";
+	cin >> numero;
 	
-	cout<<"Tabla de multiplicar del "<<numero<<":"<<endl;
-	for (int i=1;i<11;i++){
-		cout<<numero<<"x"<<i<<"="<<numero*i<<endl;
+	cout << "Tabla de multiplicar del " << numero << ":" << endl;
+	
+	for (int i=1; i<11; i++){
+		cout << numero << "x" << i << " = " << numero*i << endl;
 	}
-	return 0;
-	
 }
