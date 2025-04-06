f7c7533a74ae32d506595bf484d8cdc051f8de28
Francisco Natareno <fdeleonn2@miumg.edu.gt>
2025-04-05 22:33:29 -0600

    Corrección de estructura del programa 7

--- programas/josseline/programa7.cpp
+++ programas/josseline/programa7.cpp
@@ -1,11 +1,11 @@
 /*	
-*	Programa que convierte de números enteros con decimal a letras
+*	Programa que convierte de nÃºmeros enteros con decimal a letras
 *	
-*	Universidad Mariano Gálvez de Guatemala
-*	Facultad de Ingeniería en Sistemas
+*	Universidad Mariano GÃ¡lvez de Guatemala
+*	Facultad de IngenierÃ­a en Sistemas
 *	
 *	Autor:
-*	Josseline Adriana Catalán Juárez
+*	Josseline Adriana CatalÃ¡n JuÃ¡rez
 */
 
 #include <iostream>
@@ -13,16 +13,14 @@
 #include <sstream>
 #include <map>
 
-void programaSiete()
-{
-}
+using namespace std;
 
-std::string convertirNumeroALetras(int numero) {
-    std::map<int, std::string> palabras = {
+string convertirNumeroALetras(int numero) {
+    map<int, string> palabras = {
         {0, "cero"}, {1, "uno"}, {2, "dos"}, {3, "tres"}, {4, "cuatro"},
         {5, "cinco"}, {6, "seis"}, {7, "siete"}, {8, "ocho"}, {9, "nueve"},
         {10, "diez"}, {11, "once"}, {12, "doce"}, {13, "trece"}, {14, "catorce"},
-        {15, "quince"}, {16, "dieciséis"}, {17, "diecisiete"}, {18, "dieciocho"}, {19, "diecinueve"},
+        {15, "quince"}, {16, "diecisï¿½is"}, {17, "diecisiete"}, {18, "dieciocho"}, {19, "diecinueve"},
         {20, "veinte"}, {30, "treinta"}, {40, "cuarenta"}, {50, "cincuenta"},
         {60, "sesenta"}, {70, "setenta"}, {80, "ochenta"}, {90, "noventa"}
     };
@@ -37,22 +35,23 @@ std::string convertirNumeroALetras(int numero) {
     return palabras[decenas] + (unidades ? " y " + palabras[unidades] : "");
 }
 
-std::string convertirDecimalALetras(std::string decimal) {
-    std::string resultado = "punto";
+string convertirDecimalALetras(string decimal) {
+    string resultado = "punto";
+
     for (char digito : decimal) {
         resultado += " " + convertirNumeroALetras(digito - '0');
     }
     return resultado;
 }
 
-std::string convertirNumeroCompleto(std::string numero) {
-    std::stringstream ss(numero);
-    std::string enteroStr, decimalStr;
-    std::getline(ss, enteroStr, '.');
-    std::getline(ss, decimalStr);
+string convertirNumeroCompleto(string numero) {
+    stringstream ss(numero);
+    string enteroStr, decimalStr;
+    getline(ss, enteroStr, '.');
+    getline(ss, decimalStr);
 
     int entero = std::stoi(enteroStr);
-    std::string resultado = convertirNumeroALetras(entero);
+    string resultado = convertirNumeroALetras(entero);
 
     if (!decimalStr.empty()) {
         resultado += " " + convertirDecimalALetras(decimalStr);
@@ -61,14 +60,12 @@ std::string convertirNumeroCompleto(std::string numero) {
     return resultado;
 }
 
-int main() {
-    std::string numero;
-    std::cout << "Introduce un numero decimal: ";
-    std::cin >> numero;
+void programaSiete()
+{
+    string numero;
+    cout << "Introduce un numero decimal: ";
+    cin >> numero;
 
-    std::string resultado = convertirNumeroCompleto(numero);
-    std::cout << "El numero en letras es: " << resultado << std::endl;
-
-    return 0;
+    string resultado = convertirNumeroCompleto(numero);
+    cout << "El numero en letras es: " << resultado << endl;
 }
-
